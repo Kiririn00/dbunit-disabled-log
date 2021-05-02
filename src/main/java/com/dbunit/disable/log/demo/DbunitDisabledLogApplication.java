2576ff219dd6d83e25eb70591009ff6326f09413
Asawavetvutt Varit <as.varit_st@tni.ac.th>
2021-05-02 14:07:40 +0900

    [add] implement spring boot

---
+++ src/main/java/com/dbunit/disable/log/demo/DbunitDisabledLogApplication.java
@@ -0,0 +1,13 @@
+package com.dbunit.disable.log.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbunitDisabledLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbunitDisabledLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dbunit/disable/log/demo/DbunitDisabledLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbunit.disable.log.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbunitDisabledLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
