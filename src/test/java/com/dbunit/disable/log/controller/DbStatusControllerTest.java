279e1d073c43b28d4b30485b520f0c831d503c96
Asawavetvutt Varit <as.varit_st@tni.ac.th>
2021-05-02 17:25:43 +0900

    [add] implement dbunit

---
+++ src/test/java/com/dbunit/disable/log/controller/DbStatusControllerTest.java
@@ -0,0 +1,30 @@
+package com.dbunit.disable.log.controller;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class
+})
+class DbStatusControllerTest {
+
+    @Test
+    @Transactional
+    @DisplayName("expected customer data should be same as actual data")
+    @DatabaseSetup("classpath:customerExpectedData.xml")
+    void selectTargetData(){
+    }
+}
